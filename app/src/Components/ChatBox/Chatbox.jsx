b47e0f78af6014623174214a8618dd4bb3d9e9a5
ov3rflw <louis.arounso@yandex.com>
2025-02-05 16:53:36 +0100

    trigger websocket event

--- app/src/Components/ChatBox/Chatbox.jsx
+++ app/src/Components/ChatBox/Chatbox.jsx
@@ -1,7 +1,49 @@
+import { useEffect, useState } from "react";
+import "./Chatbox.css";
+import { io } from "socket.io-client";
+
 export default function Chatbox(){
+
+    const [socket, setSocket] = useState();
+    const [message, setMessage] = useState('');
+    const [inputValue, setInputValue] = useState('');
+
+    
+    useEffect(() => {
+        const newSocket = io('http://localhost:4000/')
+        setSocket(newSocket);
+        
+        newSocket.on("broadcastMessage", (message) => {
+            setMessage((prevMessage) => [...prevMessage, message])
+        })
+        
+        return () => newSocket.disconnect();
+    },[])
+    
+    const sendMessage = () => {
+        if (message.trim() && socket) {
+            socket.emit("sendMessage", { senderId: 1, message });
+            setMessages((prevMessages) => [...prevMessages, { senderId: 1, message }]);
+            setMessage("");
+        }
+    };
+
     return(
-        <div className="main__messages--tchatBox" style={{ flex: 2}}>
-            test
+        <div className="tchatBox" style={{ flex: 2}}>
+            <div className="tchatBox__messages">
+                
+            </div>
+            <div className="tchatBox__sendMessages">
+            <form onSubmit={sendMessage}>
+                <input
+                    type="text"
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)}
+                />
+                <button type="submit">Envoyer</button>
+            </form>
+
+            </div>
         </div>
     )
 }
---
+++ prisma/migrations/20250205151725_init/migration.sql
@@ -0,0 +1,32 @@
+/*
+  Warnings:
+
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `createdAt` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `password` on the `User` table. All the data in the column will be lost.
+  - You are about to alter the column `id` on the `User` table. The data in that column could be lost. The data in that column will be cast from `VarChar(191)` to `Int`.
+
+*/
+-- AlterTable
+ALTER TABLE `User` DROP PRIMARY KEY,
+    DROP COLUMN `createdAt`,
+    DROP COLUMN `password`,
+    MODIFY `id` INTEGER NOT NULL AUTO_INCREMENT,
+    ADD PRIMARY KEY (`id`);
+
+-- CreateTable
+CREATE TABLE `Message` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `senderId` INTEGER NOT NULL,
+    `receiverId` INTEGER NOT NULL,
+    `message` VARCHAR(191) NOT NULL,
+    `timestamp` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Message` ADD CONSTRAINT `Message_senderId_fkey` FOREIGN KEY (`senderId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Message` ADD CONSTRAINT `Message_receiverId_fkey` FOREIGN KEY (`receiverId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
