bada803bc336d84f5118445a148216f74c97c940
ov3rflw <louis.arounso@yandex.com>
2026-05-12 17:04:18 +0200

    [fix] update prisma connector

--- dataviser/app/api/messages/route.js
+++ dataviser/app/api/messages/route.js
@@ -1,7 +1,5 @@
-import { PrismaClient } from '@prisma/client';
 import { NextResponse } from 'next/server';
-
-const prisma = new PrismaClient();
+import { prisma } from '../../src/lib/prisma';
 
 export async function GET(req) {
   const { searchParams } = new URL(req.url);
